e91288aec55a7c28e865b4de6a9ebe2efe12d68e
Joshua Friedman <4homeworkstuff@gmail.com>
2020-12-05 17:16:39 -0500

    count categories

--- count.py
+++ count.py
@@ -17,22 +17,31 @@ def count(in_file):
     return counter
 
 
-################
-input_files=["./data/output-1.tsv"] #name of files with tsv data
-output_file = "./data/counted.tsv"
-#################
+
+def main():
+    ################
+    input_files=["./data/annotated-1.tsv","./data/annotated-2.tsv","./data/annotated-3.tsv"] #name of files with tsv data
+    output_file = "./data/counted.tsv"
+    #################
 
 
-counter=[0]*8
+    counter=[0]*8
 
-for fname in input_files:
-    temp=count(fname)
+    for fname in input_files:
+        temp=count(fname)
+        for i in range(8):
+            counter[i]+=temp[i]
+
+    #write to file
+    tsv_write = open(output_file,'w')
+    topics=['Legitimacy of the election','COVID-19','Transition/Concession','Economy','International politics','Climate change','Trump bad','miscellaneous topic']
     for i in range(8):
-        counter[i]+=temp[i]
+        tsv_write.write(topics[i]+'\t'+str(counter[i])+'\n')
+    tsv_write.close()
 
-#write to file
-tsv_write = open(output_file,'w')
-topics=['Legitimacy of the election','COVID-19','Transition/Concession','Economy','International politics','Climate change','Trump bad','miscellaneous topic']
-for i in range(8):
-    tsv_write.write(topics[i]+'\t'+str(counter[i])+'\n')
-tsv_write.close()
+
+
+
+
+if __name__=='__main__':
+    main()
